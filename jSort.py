c25443b48a07289cd7f5a7509ba4a7e7292fb9a0
Jordan Streete <jordan.streete@gmail.com>
2019-04-02 12:24:17 +0200

    Add files via upload

---
+++ jSort.py
@@ -0,0 +1,13 @@
+def jSort(arr, arr1, maxi):
+	for i in range(0, maxi + 1):
+		for j in range(0, len(arr)):
+			if arr[j] == i:
+				arr1.append(arr[j])
+				print(arr1)
+
+testArr = [2, 5, 7, 9, 3, 4, 6, 8, 1]
+finArr = []
+eSort(testArr, finArr, 9)
+print(finArr)
+
+#I guess this would have a time complexity best/worst/avg case of O(n^2) and space complexity of O(n)? IDK.
